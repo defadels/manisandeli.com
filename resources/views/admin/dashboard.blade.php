f5b052f36677453b47319ba055ede07eba6fad75
Muhammad Fadhil Adha <fadhil.sonic@gmail.com>
2022-07-26 17:27:47 +0700

    view scaffolding for backend dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,12 +1,1 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h2>Yea, {{ $name }}</h2>
-</body>
-</html>
+@extends('layout.admin_layout')
